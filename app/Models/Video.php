cc034966fd44f946c9bf2bc145be6cfb6e163fa9
RodrigoDeNA <rodrigovich619@gmail.com>
2026-07-06 21:21:13 +0200

    actualizo el modelo de la tabla videos autogenerada.

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Video extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'titulo',
+        'descripcion',
+        'url_video',
+        'genero',
+        'tema',
+        'signante',
+        'rango_edad',
+        'sexo',
+        'duracion',
+        'anho',
+    ];
 }
