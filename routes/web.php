b1907395c84f3c25287bfaaaa1d7590bcd88c38b
RodrigoDeNA <rodrigovich619@gmail.com>
2026-07-06 21:20:33 +0200

    configuro la conexion lavarel-mariadb

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Comprobar conexion con BD
+/* use Illuminate\Support\Facades\DB;
+
+Route::get('/test-db', function () {
+    try {
+        DB::connection()->getPdo();
+        $version = DB::select('SELECT VERSION() as version');
+        return "✅ Conexión exitosa a MariaDB. Versión: " . $version[0]->version;
+    } catch (\Exception $e) {
+        return "❌ Error de conexión: " . $e->getMessage();
+    }
+}); */
